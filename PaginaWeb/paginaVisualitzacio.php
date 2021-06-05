c5dab9282c7cd01e78589e4364f236159312810a
Biel Comas Saavedra <bielcomas1@gmail.com>
2021-06-06 00:24:47 +0200

    pagina visualitzacio arreglada i login implementat

--- PaginaWeb/paginaVisualitzacio.php
+++ PaginaWeb/paginaVisualitzacio.php
@@ -1,3 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Pagina Visualització</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</head>
+
+<body>
 
 <?php 
 $url = "https://nurseapp-b4a04.firebaseio.com/LlistatVideos.json";
@@ -10,11 +23,18 @@ $response = curl_exec($ch);
 curl_close($ch);
 
 $data = json_decode($response,true);
+
 foreach ($data as $key => $value) {
-	echo "<iframe width='560' height='315' src='https://www.youtube.com/embed/".$data[$key]["urlVideo"]."'title='YouTube video player' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe> \n";
-	echo "<br>";
-	echo "<br>";
+	if($key != 0){
+		echo "<iframe width='560' height='315' src='https://www.youtube.com/embed/".$data[$key]["urlVideo"]."'title='YouTube video player' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe> \n";
+		echo "<br>";
+		echo "<button type='button' class='btn btn-dark'>Esborrar</button> \n";
+		echo "<button type='button' class='btn btn-dark'>Amagar</button>";
+		echo "<br>";
+		echo "<br>";
+	}
 }
 
 
-?> 
+?>
+</body>
