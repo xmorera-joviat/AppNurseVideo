72c843e188185b8557f177dcdac8692e1287c005
Biel Comas Saavedra <bielcomas1@gmail.com>
2021-06-06 16:03:32 +0200

    esborrar videos firebase implementat

--- PaginaWeb/paginaVisualitzacio.php
+++ PaginaWeb/paginaVisualitzacio.php
@@ -10,10 +10,39 @@
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </head>
 
-<body>
+<script src="https://www.gstatic.com/firebasejs/7.15.5/firebase-app.js"></script>
+<script src="https://www.gstatic.com/firebasejs/7.15.5/firebase-auth.js"></script>
+<script src="https://www.gstatic.com/firebasejs/7.15.5/firebase-database.js"></script> 
 
+
+<body>
+<script src="https://www.gstatic.com/firebasejs/7.15.5/firebase-app.js"></script>
+<script src="https://www.gstatic.com/firebasejs/7.15.5/firebase-auth.js"></script>
+<script src="https://www.gstatic.com/firebasejs/7.15.5/firebase-database.js"></script> 
+
+<script>
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyCe7NPMFUTamWIH9fdk0xjk9pj3KllxkrI",
+    authDomain: "nurseapp-b4a04.firebaseapp.com",
+    databaseURL: "https://nurseapp-b4a04.firebaseio.com",
+    projectId: "nurseapp-b4a04",
+    storageBucket: "nurseapp-b4a04.appspot.com",
+    messagingSenderId: "243441860725",
+    appId: "1:243441860725:web:47745a69e80569c627b83c",
+    measurementId: "G-HL8B5LYF0W"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+</script>
+
+<div id="txtHint"></div>
+<br>
+<div>
 <?php 
-$url = "https://nurseapp-b4a04.firebaseio.com/LlistatVideos.json";
+$url = "https://nurseapp-b4a04.firebaseio.com/LlistatVideosBiel.json";
 
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL,$url);
@@ -25,16 +54,23 @@ curl_close($ch);
 $data = json_decode($response,true);
 
 foreach ($data as $key => $value) {
-	if($key != 0){
-		echo "<iframe width='560' height='315' src='https://www.youtube.com/embed/".$data[$key]["urlVideo"]."'title='YouTube video player' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe> \n";
+	if($value != null){
+		$id = $data[$key]["numId"];
+		echo "\n <iframe width='560' height='315' src='https://www.youtube.com/embed/".$data[$key]["urlVideo"]."'title='YouTube video player' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe> \n";
 		echo "<br>";
-		echo "<button type='button' class='btn btn-dark'>Esborrar</button> \n";
-		echo "<button type='button' class='btn btn-dark'>Amagar</button>";
+		echo "<p><button type='button' class='btn btn-dark' onclick='Esborrar($id)'>Esborrar</button> \n";
 		echo "<br>";
 		echo "<br>";
 	}
 }
-
-
 ?>
+</div>
+
+<script>
+function Esborrar(id){
+	
+	firebase.database().ref('LlistatVideosBiel/'+id).remove();
+}
+</script>
+
 </body>
