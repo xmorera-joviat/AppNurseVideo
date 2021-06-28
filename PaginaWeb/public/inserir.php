0f8c50e56a7ff4deb8f6ef226eb427e38d54e35c
xmorera-joviat <xmorera@joviat.cat>
2021-06-28 23:35:05 +0200

    pàgina visualitzar i penh¡jar vídeos actualitzades

--- PaginaWeb/public/inserir.php
+++ PaginaWeb/public/inserir.php
@@ -1,228 +1,243 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-  <title>Pagina Inserció</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-</head>
-<body>
+  <head>
+    <title>Pagina Inserció</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+	  	<script src="https://www.gstatic.com/firebasejs/7.15.5/firebase-app.js"></script>
+  	<script src="https://www.gstatic.com/firebasejs/7.15.5/firebase-auth.js"></script>
+  	<script src="https://www.gstatic.com/firebasejs/7.15.5/firebase-database.js"></script> 
+    <script>
+      // Your web app's Firebase configuration
+      // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+      var firebaseConfig = {
+        apiKey: "AIzaSyCe7NPMFUTamWIH9fdk0xjk9pj3KllxkrI",
+        authDomain: "nurseapp-b4a04.firebaseapp.com",
+        databaseURL: "https://nurseapp-b4a04.firebaseio.com",
+        projectId: "nurseapp-b4a04",
+        storageBucket: "nurseapp-b4a04.appspot.com",
+        messagingSenderId: "243441860725",
+        appId: "1:243441860725:web:47745a69e80569c627b83c",
+        measurementId: "G-HL8B5LYF0W"
+      };
+      // Initialize Firebase
+      firebase.initializeApp(firebaseConfig);
+      firebase.analytics();
+    </script>
+    
+    <style>
+      div.centrar {
+      	color: #FFFFFF;
+      	text-align: center;
+      }
+      body {
+      	   background-color: #4d4d4d;
+     	   color: #FFFFFF;
+      }
+    </style>
 
-	<script src="https://www.gstatic.com/firebasejs/7.15.5/firebase-app.js"></script>
-	<script src="https://www.gstatic.com/firebasejs/7.15.5/firebase-auth.js"></script>
-	<script src="https://www.gstatic.com/firebasejs/7.15.5/firebase-database.js"></script> 
-<script>
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  var firebaseConfig = {
-    apiKey: "AIzaSyCe7NPMFUTamWIH9fdk0xjk9pj3KllxkrI",
-    authDomain: "nurseapp-b4a04.firebaseapp.com",
-    databaseURL: "https://nurseapp-b4a04.firebaseio.com",
-    projectId: "nurseapp-b4a04",
-    storageBucket: "nurseapp-b4a04.appspot.com",
-    messagingSenderId: "243441860725",
-    appId: "1:243441860725:web:47745a69e80569c627b83c",
-    measurementId: "G-HL8B5LYF0W"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  firebase.analytics();
-</script>
-
-<style>
-div.centrar {
-	color: #FFFFFF;
-	text-align: center;
-}
-</style>
-<style>
-body {
-background-color: #4d4d4d;
-color: #FFFFFF;
-}
-</style>
-
-<br>
-<div class="centrar">
-<h1>Inserir nou Vídeo</h1>
-</div>
-
-<br> 
-<br>
-<!--formulari-->
-<div class='row'>
-
-<div class='col'>
-<b>Videos en Català:</b>
-<br>
-<b>Categoria:</b> 
-  <br>  
-<input id = "categoryCa" type="text" size="60" maxlength="40" placeholder="Ingressa una categoria"> 
-<br>
-<br>	
-<b>Descripció: </b>
-<br>
-<input id = "descripcioCa" type="text" size="60" maxlength="40" placeholder="descripció"> 
-<br>
-<br>
-<b> Títol: </b>
-<br>
-<input id = "titolCa" type="text" size="60" maxlength="40" placeholder="títol"> 
-</div>
-
-<div class='col'>
-<b> Vídeo en castellà:</b>
- <br>
-<b> Categoría:</b> 
-<br>
-<input id = "categoryEs" type="text" size="60" maxlength="40" placeholder="Ingresa una categoria">
-<br>
-<br>
-<b>Descripción:</b>
-<br>
-<input id = "descripcioEs" type="text" size="60" maxlength="40" placeholder="descripción">
-<br>
-<br>
-<b>Título:</b>
-<br>
-<input id = "titolEs" type="text" size="60" maxlength="40" placeholder="título">
-<br>
-<br>
-</div>
-
-<div class='col'>
-<b> Video in english: </b>	
-<br>
-<b> Category: </b>
-<br>
-<input id = "categoryEn" type="text" size="60" maxlength="40" placeholder="Ingresa una categoria">
-<br>
-<br>
-<b> Description: </b>
-<br>
-<input id = "descripcioEn" type="text" size="60" maxlength="40" placeholder="description">
-<br>
-<br>
-<b> Title: </b>
-<br>
-<input id = "titolEn" type="text" size="60" maxlength="40" placeholder="title">
-<br>
-<br>
-</div>
-</div>
-<div class ="centrar">
-  <div class="form-group">
-    <b for="pwd" style="color:#FFFFFF;">Url:</b>
-	<br>
-    <input id = "url" type="text" size="200" placeholder="Url">
-  </div>
-</div>
-<div class="centrar">
-<!--Php-->
-<?php 
-$url = "https://nurseapp-b4a04.firebaseio.com/LlistatVideosCa.json";
-
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL,$url);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-
-$response = curl_exec($ch);
-curl_close($ch);
-
-$data = json_decode($response,true);
-$id = 0;
-foreach ($data as $key => $value) {
-		$id++;
-		
-	}
-	echo "AMAGAR VÍDEO<input type='checkbox' id='amagat'>";
-	echo "<br>";
-	echo "<br>";
-	echo "<button type='button' class='btn btn-primary btn-lg' onclick='Inserir(url.value,$id)'>Inserir</button>";
-
-?>
-		
-<br>
-<br>
-
-<div id="txtHint"></div>
-<br>
-<br>
-<button type='button' class='btn btn-primary btn-lg' onclick="window.location.href = 'paginaVisualitzacio.php'">Torna a la Visualització</button>
-</div>
-
-
-<!--Funcions-->
-  <script>
-function Inserir(url,id){
+  </head>
+  
+  <body>
+  
+    <br />
+    <div class="centrar">
+    	   <h1>Inserir nou Vídeo</h1>
+    </div>
+  
+  	<br /> 
+    <br />
+    <!--formulari-->
+    <div class='row'> 
+      <div class='col'>
+        <b>Català:</b>
+        <br />
+		<b> Títol: </b>
+        <br />
+        <input id = "titolCa" type="text" size="60" maxlength="40" placeholder="títol">
+        <br /><br />
+        <b>Descripció: </b>
+        <br />
+		<textarea id = "descripcioCa" rows="4" cols="63"></textarea> 
+        <br /><br />
+        <b>Categoria:</b> 
+        <br />  
+        <input id = "categoryCa" type="text" size="60" maxlength="40" placeholder="categoria"> 
+        <br /><br /> 
+      </div>
+    
+      <div class='col'>
+        <b> Español:</b>
+        <br />
+  	  	<b>Título:</b>
+        <br />
+        <input id = "titolEs" type="text" size="60" maxlength="40" placeholder="título">   
+        <br />
+        <br />
+        <b>Descripción:</b>
+        <br />
+		<textarea id = "descripcioEs" rows="4" cols="63"></textarea>
+        <br />
+        <br />
+        <b> Categoría:</b> 
+        <br />
+        <input id = "categoryEs" type="text" size="60" maxlength="40" placeholder="categoría">
+        <br />
+        <br />
+      </div>
+      
+      <div class='col'>
+        <b> English: </b>	
+        <br />
+  	  	<b> Title: </b>
+        <br />
+ 		<input id = "titolEn" type="text" size="60" maxlength="40" placeholder="title"> 
+        <br />
+        <br />
+        <b> Description: </b>
+        <br />
+		<textarea id = "descripcioEn" rows="4" cols="63"></textarea>
+<!--        <input id = "descripcioEn" type="text" size="60" maxlength="58" placeholder="description"> -->
+        <br />
+        <br />
+        <b> Category: </b>
+        <br />
+        <input id = "categoryEn" type="text" size="60" maxlength="40" placeholder="category">
+        <br />
+        <br />
+      </div>
+    </div>
+	
+    <div class ="centrar">
+      <div class="form-group">
+        <b for="pwd" style="color:#FFFFFF;">Url:</b>
+    	<br />
+        <input id = "url" type="text" size="100" placeholder="Url">
+      </div>
+    </div>
+	
+    <div class="centrar">
+      <?php
+	  
+	  //fem un recorregut de la base de dades per obtenir l'index (revisar aquest mètode i optimitzar)
+        $url = "https://nurseapp-b4a04.firebaseio.com/LlistatVideosCa.json";
+        
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL,$url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        
+        $response = curl_exec($ch);
+        curl_close($ch);
+        
+        $data = json_decode($response,true);
+        $id = 0;
+        foreach ($data as $key => $value) {
+        		$id++;	
+        	}
 			
-		var x = document.getElementById("amagat");
-		var mostrar = 1;
-		if(x.checked == true){
-			mostrar = 0;
-		}
-		//variables de text.
-		var catCa, descCa,titCa;
-		var catEs, descEs, titEs;
-		var catEn, descEn, titEn;
-		
-		//variables català
-		catCa = document.getElementById('categoryCa').value;
-		descCa = document.getElementById("descripcioCa").value;
-		titCa = document.getElementById("titolCa").value;
-		
-		//variables castella
-		catEs = document.getElementById("categoryEs").value;
-		
-		descEs = document.getElementById("descripcioEs").value;
-		titEs = document.getElementById("titolEs").value;
-		
-		//variables anglès
-		catEn = document.getElementById("categoryEn").value;
-		descEn = document.getElementById("descripcioEn").value;
-		titEn = document.getElementById("titolEn").value;
-		
-		
-		if(descCa.length == 0 || titCa.length == 0 || descEs.length == 0 || titEs.length == 0 || descEn == 0 || titEn == 0 ||url.length == 0){
-			document.getElementById("txtHint").innerHTML = "Completi la informació obligatòria siusplau.";
-		}
-		else{
-			var urlFinal = url.substring(url.indexOf("watch?v=")+8,url.length);
-			if(urlFinal == ""){
-				document.getElementById("txtHint").innerHTML = "Posa un enllaç de youtube correcte. Exemple: youtube.com/watch?v=57mfjMVka";
-			}
-			else{
-				firebase.database().ref('LlistatVideosCa/'+id).set({
-					'categoria': catCa,
-					'descVideo': descCa,
-					'mostrar': mostrar,
-					'numId': id,
-					'titol': titCa,
-					'urlVideo': urlFinal
-				});
-				firebase.database().ref('LlistatVideosEs/'+id).set({
-					'categoria': catEs,
-					'descVideo': descEs,
-					'mostrar': mostrar,
-					'numId': id,
-					'titol': titEs,
-					'urlVideo': urlFinal
-				});
-				firebase.database().ref('LlistatVideosEn/'+id).set({
-					'categoria': catEn,
-					'descVideo': descEn,
-					'mostrar': mostrar,
-					'numId': id,
-					'titol': titEn,
-					'urlVideo': urlFinal
-				})
-				.then(result => window.location.reload(result));
-				document.getElementById("txtHint").innerHTML= "Inserit correctament.";
-			}
-		}
-}
-</script>
-</body>
+        echo "AMAGAR VÍDEO<input type='checkbox' id='amagat'>";
+        echo "<br />";
+        echo "<br />";
+        			
+        echo "<button type='button' class='btn btn-primary btn-lg' onclick='Inserir(url.value,$id)'>Inserir</button>";
+      ?>
+      		
+      <br />
+      <br />
+      
+      <div id="txtHint"></div>
+      <br />
+      <br />
+	  
+      <button type='button' class='btn btn-primary btn-lg' onclick="window.location.href = 'visualitzar.php'">Tornar a la Visualització</button>
+    </div>
+    
+    
+    <!--Funcions-->
+	<script>
+        function Inserir(url,id){
+        			
+        	var x = document.getElementById("amagat");
+        	var mostrar = 1;
+			var urlFinal="";
+        	if(x.checked == true){
+        		mostrar = 0;
+        	}
+        	//variables de text.
+        	var catCa, descCa,titCa;
+        	var catEs, descEs, titEs;
+        	var catEn, descEn, titEn;
+        	
+        	//variables català
+        	titCa = document.getElementById("titolCa").value;
+    		descCa = document.getElementById("descripcioCa").value;
+    		catCa = document.getElementById('categoryCa').value;
+        		
+        	//variables castella
+        	titEs = document.getElementById("titolEs").value;
+        	descEs = document.getElementById("descripcioEs").value;
+        	catEs = document.getElementById("categoryEs").value;
+        	
+        	//variables anglès
+        	titEn = document.getElementById("titolEn").value;
+        	descEn = document.getElementById("descripcioEn").value;
+        	catEn = document.getElementById("categoryEn").value;
+        		
+        	if(descCa.length == 0 || titCa.length == 0 || descEs.length == 0 || titEs.length == 0 || descEn == 0 || titEn == 0 ||url.length == 0){
+        		document.getElementById("txtHint").innerHTML = "Completi tots els camps si us plau.";
+        	}
+        	else{
+				
+				if (url.indexOf("youtu.be/")!== -1){
+        		   urlFinal = url.substring(url.indexOf("youtu.be/")+9,url.length);
+				}
+				if (url.indexOf("youtube.com/")!==-1){
+					 urlFinal = url.substring(url.indexOf("watch?v=")+8,url.length);
+				}
+				//document.getElementById("txtHint").innerHTML = urlFinal;		 
+    		
+    			
+    			
+    			
+   
+    		
+      		if(urlFinal == ""){
+        			document.getElementById("txtHint").innerHTML = "Introduir un enllaç de youtube correcte. Exemple: https://youtu.be/vX3krP6JmOY o https://youtube.com/watch?v=vX3krP6JmOY";
+        		}
+        		else{
+        			firebase.database().ref('LlistatVideosCa/'+id).set({
+        				'categoria': catCa,
+        				'descVideo': descCa,
+        				'mostrar': mostrar,
+        				'numId': id,
+        				'titol': titCa,
+        				'urlVideo': urlFinal
+        			});
+        			firebase.database().ref('LlistatVideosEs/'+id).set({
+        				'categoria': catEs,
+        				'descVideo': descEs,
+        				'mostrar': mostrar,
+        				'numId': id,
+        				'titol': titEs,
+        				'urlVideo': urlFinal
+        			});
+        			firebase.database().ref('LlistatVideosEn/'+id).set({
+        				'categoria': catEn,
+        				'descVideo': descEn,
+        				'mostrar': mostrar,
+        				'numId': id,
+        				'titol': titEn,
+        				'urlVideo': urlFinal
+        			})
+        			.then(result => window.location.reload(result));
+        			document.getElementById("txtHint").innerHTML= "Inserit correctament.";
+        		}
+        	}
+        }
+    </script>
+  </body>
+ </html>
--- PaginaWeb/public/visualitzar.php
+++ PaginaWeb/public/visualitzar.php
@@ -1,221 +1,149 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-  <title>Visualitzar Vídeos</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-</head>
-
-<body>
-<script src="https://www.gstatic.com/firebasejs/7.15.5/firebase-app.js"></script>
-<script src="https://www.gstatic.com/firebasejs/7.15.5/firebase-auth.js"></script>
-<script src="https://www.gstatic.com/firebasejs/7.15.5/firebase-database.js"></script> 
-
-<script>
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  var firebaseConfig = {
-    apiKey: "AIzaSyCe7NPMFUTamWIH9fdk0xjk9pj3KllxkrI",
-    authDomain: "nurseapp-b4a04.firebaseapp.com",
-    databaseURL: "https://nurseapp-b4a04.firebaseio.com",
-    projectId: "nurseapp-b4a04",
-    storageBucket: "nurseapp-b4a04.appspot.com",
-    messagingSenderId: "243441860725",
-    appId: "1:243441860725:web:47745a69e80569c627b83c",
-    measurementId: "G-HL8B5LYF0W"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  firebase.analytics();
-</script>
-<style>
-div.centrar {
-	color: #FFFFFF;
-	text-align: center;
-}
-</style>
-<style>
-body {
-background-color: #4D4D4D;
-color: #FFFFFF;
-}
-</style>
-
-<div id="txtHint"></div>
-<br>
-
-<div class="centrar">
-<br>
-<br>
-<button  class="btn btn-outline-success" onclick="window.location.href = 'inserir.php'" >Inserir Nou Vídeo</button>
-</div>
-<?php 
-echo "<div>";
-$urlCa = "https://nurseapp-b4a04.firebaseio.com/LlistatVideosCa.json";
-$urlEn = "https://nurseapp-b4a04.firebaseio.com/LlistatVideosEn.json";
-$urlEs = "https://nurseapp-b4a04.firebaseio.com/LlistatVideosEs.json";
-
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL,$urlCa);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-
-$response = curl_exec($ch);
-curl_close($ch);
-
-$data = json_decode($response,true);
-
-//urls finals
-$urlCaFinal = substr($urlCa, strpos($urlCa,"com/")+4, strlen($urlCa));
-$urlEsFinal = substr($urlEs, strpos($urlEs,"com/")+4, strlen($urlEs));
-$urlEnFinal = substr($urlEn, strpos($urlEn,"com/")+4, strlen($urlEn));
-echo "<div class='row'>";
-
-
-echo "<div class='col'>";
-echo "<b>Videos en Català:</b>";
-echo "<br>";
-//Videos Català
-foreach ($data as $key => $value) {
-	if($value != null){
-		$id = $data[$key]["numId"];
-		echo "\n <iframe width='560' height='315' src='https://www.youtube.com/embed/".$data[$key]["urlVideo"]."'title='YouTube video player' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe> \n";
-		echo "<br>";
-		echo "<button type='button' class='btn btn-outline-warning' onclick='Editar($id)'>Editar Dades</button> \n";
-		if($data[$key]["mostrar"] == 1){
-			echo "<button type='button' class = 'btn btn-outline-success' onclick='Amagar(0,$id)'> Amagar Vídeo</button> \n";
-		}
-		else if($data[$key]["mostrar"] == 0){
-			echo "<button type='button' class = 'btn btn-outline-success' onclick='Amagar(1,$id)'> Mostrar Vídeo</button> \n";
-		}
-		echo "<button type='button' class='btn btn-outline-danger' onclick='Esborrar($id)'>Esborrar</button> \n";
-		echo "<br>";
-		echo "<br>";
-	}
-}
-echo"</div>";
-//Videos Castellà
-$chEs = curl_init();
-curl_setopt($chEs,CURLOPT_URL,$urlEs);
-curl_setopt($chEs,CURLOPT_RETURNTRANSFER,true);
-
-$responseEs = curl_exec($chEs);
-curl_close($chEs);
-
-$dataEs = json_decode($responseEs,true);
-echo "<div class='col'>";
-echo "<b>Videos en Castellà:</b>";
-echo "<br>";
-foreach ($dataEs as $key => $valueEs) {
-	if($valueEs != null){
-		$id = $dataEs[$key]["numId"];
-		echo "\n <iframe width='560' height='315' src='https://www.youtube.com/embed/".$dataEs[$key]["urlVideo"]."'title='YouTube video player' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe> \n";
-		echo "<br>";
-		echo "<button type='button' class='btn btn-outline-warning' onclick='Editar($id)'>Editar Dades</button> \n";
-		if($dataEs[$key]["mostrar"] == 1){
-			echo "<button type='button' class = 'btn btn-outline-success' onclick='Amagar(0,$id)'> Amagar Vídeo</button> \n";
-		}
-		else if($dataEs[$key]["mostrar"] == 0){
-			echo "<button type='button' class = 'btn btn-outline-success' onclick='Amagar(1,$id)'> Mostrar Vídeo</button> \n";
-		}
-		echo "<button type='button' class='btn btn-outline-danger' onclick='Esborrar($id)'>Esborrar</button> \n";
-		echo "<br>";
-		echo "<br>";
-		}
-}
-echo "</div>";
-
-//Videos anglès.
-$chEn = curl_init();
-curl_setopt($chEn,CURLOPT_URL,$urlEn);
-curl_setopt($chEn,CURLOPT_RETURNTRANSFER,true);
-
-$responseEn = curl_exec($chEn);
-curl_close($chEn);
-
-$dataEn = json_decode($responseEn,true);
-echo "<div class='col'>";
-echo "<b> Vídeos en Anglès:</b>";
-echo "<br>";
-foreach ($dataEn as $key => $valueEn) {
-	if($valueEn != null) {
-		$id = $dataEn[$key]["numId"];
-		echo "\n <iframe width='560' height='315' src='https://www.youtube.com/embed/".$dataEn[$key]["urlVideo"]."'title='YouTube video player' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe> \n";
-		echo "<br>";
-		echo "<button type='button' class='btn btn-outline-warning' onclick='Editar($id)'>Editar Dades</button> \n";
-		if($dataEn[$key]["mostrar"] == 1){
-			echo "<button type='button' class = 'btn btn-outline-success' onclick='Amagar(0,$id)'> Amagar Vídeo</button> \n";
-		}
-		else if($dataEn[$key]["mostrar"] == 0){
-			echo "<button type='button' class = 'btn btn-outline-success' onclick='Amagar(1,$id)'> Mostrar Vídeo</button> \n";
-		}
-		echo "<button type='button' class='btn btn-outline-danger' onclick='Esborrar($id)'>Esborrar</button> \n";
-		echo "<br>";
-		echo "<br>";
-	}
-}
-echo "</div>";
-?>
-</div>
-<div class="centrar">
-<br>
-<br>
-<button  class="btn btn-primary btn-lg" onclick="SignOut()" >Sortir</button> 
-<br>
-<br>
-</div>
-<script>
-function Esborrar(id){
-	if(confirm('Estas segur que vols esborrar el vídeo?')){
-		firebase.database().ref('LlistatVideosCa/'+id).remove();
-		firebase.database().ref('LlistatVideosEn/'+id).remove();
-		firebase.database().ref('LlistatVideosEs/'+id).remove()
-			.then(result => window.location.reload(result));
-	}
-	
-}
-</script>
-<script>
-function SignOut(){
-	firebase.auth().signOut();
-	window.location.href = "index.html";
-}
-</script>
-<script>
-function Editar(id){
-	window.location.href = "editar.php?id="+id;  	
-}
-</script>
-<script>
-function Amagar(amagar,id){
-	if(amagar == 0){
-	
-		firebase.database().ref('LlistatVideosCa/'+id).update({
-					'mostrar': 0
-				});
-		firebase.database().ref('LlistatVideosEs/'+id).update({
-					'mostrar': 0
-				});
-		firebase.database().ref('LlistatVideosEn/'+id).update({
-					'mostrar': 0
-				}).then(result => window.location.reload(result));
-	}
-	else if(amagar == 1){
-		
-	
-		firebase.database().ref('LlistatVideosCa/'+id).update({
-					'mostrar': 1
-				});
-		firebase.database().ref('LlistatVideosEs/'+id).update({
-					'mostrar': 1
-				});
-		firebase.database().ref('LlistatVideosEn/'+id).update({
-					'mostrar': 1
-				}).then(result => window.location.reload(result));
-	}
-}
-</script>
-</body>
+  <head>
+    <title>Visualitzar Vídeos</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script src="https://www.gstatic.com/firebasejs/7.15.5/firebase-app.js"></script>
+  	<script src="https://www.gstatic.com/firebasejs/7.15.5/firebase-auth.js"></script>
+  	<script src="https://www.gstatic.com/firebasejs/7.15.5/firebase-database.js"></script> 
+  	<script>
+    // Your web app's Firebase configuration
+    // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  	  var firebaseConfig = {
+  	    apiKey: "AIzaSyCe7NPMFUTamWIH9fdk0xjk9pj3KllxkrI",
+  	    authDomain: "nurseapp-b4a04.firebaseapp.com",
+  	    databaseURL: "https://nurseapp-b4a04.firebaseio.com",
+  	    projectId: "nurseapp-b4a04",
+  	    storageBucket: "nurseapp-b4a04.appspot.com",
+  	    messagingSenderId: "243441860725",
+  	    appId: "1:243441860725:web:47745a69e80569c627b83c",
+  	    measurementId: "G-HL8B5LYF0W"
+  	  };
+  	  // Initialize Firebase
+  	  firebase.initializeApp(firebaseConfig);
+  	  firebase.analytics();
+  	</script>
+  	<style>
+  		div.centrar {
+  			color: #FFFFFF;
+  			text-align: center;
+  		}
+  		body {
+  		background-color: #4D4D4D;
+  		color: #FFFFFF;
+  		}
+  	</style>
+  </head>
+  
+  <body>
+  
+  <!-- comentari intern -->	<script>console.log("Prova consola html linia 46");</script>
+  
+  	<div id="txtHint"></div>
+  	<br />
+  	<div class="centrar">
+  		<br />
+  		<br />
+  		<button  class="btn btn-outline-success" onclick="window.location.href = 'inserir.php'" >Inserir Nou Vídeo</button>
+  	</div>
+  		<?php 
+  			echo "<div>";
+  			//llistat de vídeos a firebase
+  			$url = "https://nurseapp-b4a04.firebaseio.com/LlistatVideosCa.json";
+  
+  
+  
+  			$ch = curl_init();
+  			curl_setopt($ch, CURLOPT_URL,$url);
+  			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+  
+  			$response = curl_exec($ch);
+  
+  			curl_close($ch);
+  
+  			$data = json_decode($response,true);
+  
+  			//urls finals
+  			$urlFinal = substr($url, strpos($url,"com/")+4, strlen($url));
+  
+    			echo "<div class='row'>";    
+        			echo "<div class='col'>";
+            			echo "<b>Llistat de Videos:</b>";
+            			echo "<br />";
+            
+            			foreach ($data as $key => $value) {
+            				if($value != null){
+            					$id = $data[$key]["numId"];
+            					//per a cada element que conte la llista de vídeos obtenim la urlVideo i creem l'iframe corresponent
+            					echo "\n <iframe width='560' height='315' src='https://www.youtube.com/embed/".$data[$key]["urlVideo"]."'title='YouTube video player' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe> \n";
+            					
+								echo "<br />Títol: ".$data[$key]["titol"];
+							  	echo "<br />Descripció: ".$data[$key]["descVideo"];
+							  	echo "<br />Categoria: ".$data[$key]["categoria"];
+								
+								echo "<br /><br />";
+            					echo "<button type='button' disabled class='btn btn-outline-warning' onclick='editar($id)'>Editar Dades</button> \n";//botó desabilitat
+            					if($data[$key]["mostrar"] == 1){
+            						echo "<button type='button' class = 'btn btn-outline-success' onclick='amagar(0,$id)'> Amagar Vídeo</button> \n";
+            					}
+            					else if($data[$key]["mostrar"] == 0){
+            						echo "<button type='button' class = 'btn btn-outline-success' onclick='amagar(1,$id)'> Mostrar Vídeo</button> \n";
+            					}
+            					echo "<button type='button' class='btn btn-outline-danger' onclick='esborrar($id)'>Esborrar</button> \n";
+            					echo "<br />";
+            					echo "<br />";
+            				}
+            			}
+        			echo "</div>";
+				echo "</div>";    
+  		?>
+  	</div>
+  	<div class="centrar">
+  		<br />
+  		<br />
+  		<button  class="btn btn-primary btn-lg" onclick="signOut()" >Sortir</button> 
+  		<br />
+  		<br />
+  	</div>
+  	<script>
+  	function esborrar(id){
+  		if(confirm('Estas segur que vols esborrar el vídeo?')){
+  			firebase.database().ref('LlistatVideosCa/'+id).remove();
+  			firebase.database().ref('LlistatVideosEn/'+id).remove();
+  			firebase.database().ref('LlistatVideosEs/'+id).remove()
+  				.then(result => window.location.reload(result));
+  		}
+  		
+  	}
+  	</script>
+  
+  	<script>
+  	function editar(id){
+  		window.location.href = "editar.php?id="+id;  	
+  	}
+  	</script>
+  	<script>
+  	function amagar(amagar,id){
+  		if(amagar == 0){
+  			firebase.database().ref('LlistatVideosCa/'+id).update({
+  						'mostrar': 0}).then(result => window.location.reload(result));
+  		}
+  		else if(amagar == 1){
+  			firebase.database().ref('LlistatVideosCa/'+id).update({
+  						'mostrar': 1}).then(result => window.location.reload(result));
+  			}
+  		}
+  		</script>
+  		<script>
+  			function signOut(){
+  				firebase.auth().signOut();
+  				window.location.href = "index.html";
+  			}
+  	</script>
+  </body>
+</html>
